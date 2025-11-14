22a0161e96fd1345b2ac70290961a6b4b5ac223f
Nihan Ali VP <manzclinicppm@gmail.com>
2025-11-14 22:19:24 +0530

    l

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/pivalue.nihan-vp.me/', // Set base path for GitHub Pages deployment
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
