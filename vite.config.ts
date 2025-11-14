450e166136d30616aadc89bbf439b3ae310d398d
Nihan Ali VP <manzclinicppm@gmail.com>
2025-11-14 16:47:29 +0530

    fix:first

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/',
       server: {
         port: 3000,
-        host: '0.0.0.0'
+        host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
